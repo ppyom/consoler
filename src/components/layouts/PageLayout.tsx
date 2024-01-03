b0acb93d9fa6d2c787d5d35ff58f347e4094a05b
ppyom <do_ob199@naver.com>
2024-01-03 17:40:56 +0900

    Home.tsx 페이지 생성
    * Home.tsx 페이지 추가
    * PageLayout에 padding 추가

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const PageLayout: React.FC<Props> = ({ children }) => {
 	return (
-		<main className="w-full h-full flex justify-center">
+		<main className="w-full h-full flex justify-center p-2">
 			<div className="w-full max-w-screen-xl">
 				{ children }
 			</div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,21 @@
+import React from "react";
 import PageLayout from "../components/layouts/PageLayout";
+import LinkButton from "../components/common/LinkButton";
+import routes from "../routes";
 
 const Home = () => {
 	return (
 		<PageLayout>
-			Home Page
+			<div className="flex flex-col gap-2">
+				{
+					routes
+						.filter((route) => route.showInMenu)
+						.map(({ id, displayName, path }) => (
+							<LinkButton key={id} className="bg-zinc-100 hover:brightness-90 p-2 rounded-lg drop-shadow" name={displayName} to={path!} />
+						))
+				}
+			</div>
+			{/* 최근 저장한 Console 목록 출력!! */}
 		</PageLayout>
 	);
 }
