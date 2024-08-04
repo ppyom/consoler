56581eb0c9bc7f54313cd0a38f4fe53f56256892
ppyom <ouob77@kakao.com>
2024-08-04 15:19:42 +0900

    feat: Navbar 컴포넌트 추가 #1
    * Navbar 컴포넌트 스타일 설정

--- src/components/layouts/Navbar/Navbar.tsx
+++ src/components/layouts/Navbar/Navbar.tsx
@@ -1,7 +1,27 @@
 import styles from './Navbar.module.css';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
-  return <nav className={styles.nav}>nav</nav>;
+  const location = useLocation();
+  const menu = [
+    { name: 'Home', path: '/' },
+    { name: 'Make', path: '/make' },
+    { name: 'List', path: '/list' },
+  ];
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        {menu.map((item, idx) => (
+          <li
+            key={`nav_menu_${idx}`}
+            className={location.pathname === item.path ? styles.selected : ''}
+          >
+            <Link to={item.path}>{item.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
