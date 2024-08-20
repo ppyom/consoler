3d7031e7211b3d3138806e88784ef0cccc455df2
ppyom <ouob77@kakao.com>
2024-08-20 17:47:13 +0900

    feat: Viewer 컴포넌트 구현 #7
    * Viewer 내부에서 사용하는 Line, Block 생성

--- src/pages/EditPage/Viewer/Viewer.tsx
+++ src/pages/EditPage/Viewer/Viewer.tsx
@@ -1,7 +1,19 @@
+import { useConsole } from '../../../context/consoleContext.tsx';
+import Line from './Line.tsx';
 import styles from './Viewer.module.css';
 
 const Viewer = () => {
-  return <div className={styles.viewer}>Viewer</div>;
+  const { blocks, addLine } = useConsole();
+  return (
+    <div className={styles.viewer}>
+      <div className={styles.blocks}>
+        {blocks.map((line, idx) => (
+          <Line key={`line${idx}`} line={idx} blocks={line} />
+        ))}
+        <button onClick={() => addLine()}>+</button>
+      </div>
+    </div>
+  );
 };
 
 export default Viewer;
