ca16035d8803b62a2909e43b8749434ab277f9b3
ppyom <ouob77@kakao.com>
2024-08-20 16:10:50 +0900

    feat: EditPage 수정 #7
    * EditPage 내부에서 Context 를 사용할 수 있게 Provider 추가

--- src/pages/EditPage/EditPage.tsx
+++ src/pages/EditPage/EditPage.tsx
@@ -1,3 +1,5 @@
+import { useParams } from 'react-router-dom';
+import { ConsoleProvider } from '../../context/consoleContext.tsx';
 import PageLayout from '../../components/layouts/PageLayout/PageLayout.tsx';
 import InformationForm from './InformationForm/InformationForm.tsx';
 import Viewer from './Viewer/Viewer.tsx';
@@ -6,15 +8,18 @@ import Result from './Result/Result.tsx';
 import styles from './EditPage.module.css';
 
 const EditPage = () => {
+  const { id } = useParams();
   return (
-    <PageLayout className={styles.edit}>
-      <InformationForm />
-      <div className={styles.editor}>
-        <Viewer />
-        <Editor />
-      </div>
-      <Result />
-    </PageLayout>
+    <ConsoleProvider id={id}>
+      <PageLayout className={styles.edit}>
+        <InformationForm />
+        <div className={styles.editor}>
+          <Viewer />
+          <Editor />
+        </div>
+        <Result />
+      </PageLayout>
+    </ConsoleProvider>
   );
 };
 
